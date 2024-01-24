85ee4fcb4de9a7ef66e0cfaa95c9717c1feb5429
Vincent Tsang <vincent168e@gmail.com>
2024-01-24 15:52:42 +0800

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const CreateIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1)
+  title: z.string().min(1, 'Title is required.').max(255),
+  description: z.string().min(1, 'Description is required.')
 })
 
 export async function POST(request: NextRequest) {
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
   const valiation = CreateIssueSchema.safeParse(body);
 
   if (!valiation.success)
-    return NextResponse.json(valiation.error.errors, { status: 400 });
+    return NextResponse.json(valiation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description }
