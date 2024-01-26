a05e95227b2523f6850f84cd7ec687228a338557
Vincent Tsang <vincent168e@gmail.com>
2024-01-26 16:07:40 +0800

    Secure the api of application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,16 @@
 import { IssueSchema } from "@/app/ValidationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 export async function PATCH(
   request: NextRequest, 
   { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return NextResponse.json({}, { status: 401 });
+  
   const body = await request.json();
   const validation = IssueSchema.safeParse(body);
   if (!validation.success)
@@ -30,6 +36,9 @@ export async function PATCH(
 export async function DELETE(
   request: NextRequest, 
   { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return NextResponse.json({}, { status: 401 });
   
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
