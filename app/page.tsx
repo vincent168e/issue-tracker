79f881b56bac8d6234dfeee414ed0ccb339eca9c
Vincent Tsang <vincent168e@gmail.com>
2024-01-27 21:29:27 +0800

    Build the IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/client";
 
@@ -12,5 +13,5 @@ export default async function Home() {
     where: { status: "CLOSED" },
   });
 
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
