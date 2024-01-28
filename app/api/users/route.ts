05e47b75dadc94817be4f75612dde6a69274a87b
Vincent Tsang <vincent168e@gmail.com>
2024-01-29 00:19:05 +0800

    Add relationMode to prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
 
 export async function GET(request: NextRequest) {
   const users = await prisma?.user.findMany({ orderBy: { name: 'asc' } });
