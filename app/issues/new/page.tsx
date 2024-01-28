eb6d84b28b1301167ec2a26538d77ed78790fae5
Vincent Tsang <vincent168e@gmail.com>
2024-01-29 00:49:32 +0800

    Fix dynamic async

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,13 @@
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
 
-const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
-  ssr: false,
-  loading: () => <IssueFormSkeleton />,
-});
+const IssueForm = dynamic(
+  async () => import("@/app/issues/_components/IssueForm"),
+  {
+    ssr: false,
+    loading: () => <IssueFormSkeleton />,
+  }
+);
 
 const NewIssuePage = () => {
   return <IssueForm />;
